9031aef7835dea69f4f11593beeec2633cb03c3a
Fabest Valencia <fabiane.valenciaa@uqvirtual.edu.co>
2026-06-04 16:52:51 -0500

    Proyecto inicial JavaFX

---
+++ src/main/java/com/example/gestionfacturas/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.gestionfacturas;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
